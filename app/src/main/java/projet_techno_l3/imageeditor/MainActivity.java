2124c886073acf4dce9c65b9da0e5948e28f3340
Cyril <cyril.niobe@gmail.com>
2017-03-05 21:38:49 +0100

    Changed save dialog color style

--- app/src/main/java/projet_techno_l3/imageeditor/MainActivity.java
+++ app/src/main/java/projet_techno_l3/imageeditor/MainActivity.java
@@ -130,7 +130,6 @@ public class MainActivity extends AppCompatActivity {
         onMenuBackClicked(null);
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
