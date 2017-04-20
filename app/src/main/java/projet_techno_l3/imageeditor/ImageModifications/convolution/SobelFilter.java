8ebfd20ee3dcf4ddeb2e22ed2529e9515975ff2e
Cyril <cyril.niobe@gmail.com>
2017-04-20 21:19:50 +0200

    Added greyscale before Sobel

--- app/src/main/java/projet_techno_l3/imageeditor/ImageModifications/convolution/SobelFilter.java
+++ app/src/main/java/projet_techno_l3/imageeditor/ImageModifications/convolution/SobelFilter.java
@@ -36,10 +36,22 @@ public class SobelFilter extends AbstractImageModificationAsyncTask {
     protected Bitmap doInBackground(String... params) {
         try {
 
-            Bitmap result = src.copy(Bitmap.Config.ARGB_8888, true);
-            int imgHeight = result.getHeight();
-            int imgWidth = result.getWidth();
+            //first: greyscale image
+            int imgHeight = src.getHeight();
+            int imgWidth = src.getWidth();
 
+            Bitmap result = src.copy(Bitmap.Config.ARGB_8888, true);
+
+            int[] pixels = new int[imgWidth * imgHeight];
+            result.getPixels(pixels, 0, imgWidth, 0, 0, imgWidth, imgHeight);
+
+            for (int i = 0; i < imgHeight * imgWidth; i++) {
+                int pixel = pixels[i];
+                pixels[i] = greyOutPixel(pixel);
+            }
+            result.setPixels(pixels, 0, imgWidth, 0, 0, imgWidth, imgHeight);
+
+            //then sobel filter
             for (int x = 1; x < imgWidth-1; x++) {
                 for (int y = 1; y < imgHeight-1; y++) {
                     convolutionMatrix[0][0]=Color.green(src.getPixel(x-1,y-1));
