64394c827f3c06a787b5df4010535c57b1cca4a7
Cyril <cyril.niobe@gmail.com>
2017-04-20 19:04:52 +0200

    Fixed Laplacian filter by using openCV

--- app/src/main/java/projet_techno_l3/imageeditor/ImageModifications/convolution/LaplacianFilter.java
+++ app/src/main/java/projet_techno_l3/imageeditor/ImageModifications/convolution/LaplacianFilter.java
@@ -4,79 +4,36 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
 import projet_techno_l3.imageeditor.ImageModifications.AbstractImageModificationAsyncTask;
 
 /**
  * Applies a Laplacian filter on the image
  */
 public class LaplacianFilter extends AbstractImageModificationAsyncTask {
-    //private Callable greyScaleCallable;
-    private int[][] convolutionMatrix = {
-            {1,1,1},
-            {1,-8,1},
-            {1,1,1}
-    };
-
     /**
      * Constructor
      * @param src bitmap to be modified
      */
     public LaplacianFilter(Bitmap src, Activity activity) {
         super(src, activity);
-
-        //greyScaleCallable = new Greyscale(this.src);
     }
 
     /**
-     * Performs the operation on a specific thread
+     * Performs the operation on a specific thread by using the Laplacian method provided by openCV
      * @return result bitmap
      */
     @Override
     protected Bitmap doInBackground(String... params) {
-        int r,g,b;
-        int rtotal, gtotal, btotal; rtotal = gtotal = btotal = 0;
-
         Bitmap result = src.copy(Bitmap.Config.ARGB_8888, true);
-        //result = (Bitmap) greyScaleCallable.call();
-
-        for(int i = 1; i<result.getWidth()-1; i++) {
-            for (int j = 1; j < result.getHeight() - 1; j++) {
-                for (int y = -1; y <= 1; y++)
-                {
-                    for (int x = -1; x <= 1; x++)
-                    {
-                        // get each channel pixel value
-                        int pixel = result.getPixel(i+x,j+y);
-
-                        r = Color.red(pixel);
-                        g = Color.green(pixel);
-                        b = Color.blue(pixel);
-
-                        // calculate each channel surrouding neighbour pixel value base
-                        rtotal += r * convolutionMatrix[y + 1][x + 1];
-                        gtotal += g * convolutionMatrix[y + 1][x + 1];
-                        btotal += b * convolutionMatrix[y + 1][x + 1];
-                    }
-
-                }
-
-
-                if (rtotal > 255) rtotal = 255;
-
-                else if (rtotal < 0) rtotal = 0;
-
-                if (btotal > 255) btotal = 255;
-
-                else if (btotal < 0) btotal = 0;
-
-                if (gtotal > 255) gtotal = 255;
-
-                else if (gtotal < 0) gtotal = 0;
-
-                // commit new pixel value
-                result.setPixel(i,j,Color.rgb(rtotal,btotal,gtotal));
-            }
-        }
+        Mat mat = new Mat();
+        Utils.bitmapToMat(result,mat);
+        Imgproc.Laplacian(mat, mat, CvType.CV_8U, 3, 1, 0);
+        Utils.matToBitmap(mat,result);
         return result;
     }
 }
